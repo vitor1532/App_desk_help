d1ac928a68efb7b1d504bea954ae89b92848a0bb
unknown <vitomartinsalmeida@gmail.com>
2021-08-10 16:58:50 -0300

    criando arr de usuarios

--- valida_login.php
+++ valida_login.php
@@ -1,5 +1,16 @@
 <?php
 	
+	//usuarios do sistema
+	$usuarios_app = [
+		array('email' => 'adm@teste.com', 'senha' => '123456'),
+		array('email' => 'user@teste.com', 'senha' => 'abcd'),
+		array('email' => 'vitor@teste.com', 'senha' => '456789'),
+	];
+
+	echo '<pre>';
+	print_r($usuarios_app);
+	echo '</pre>';
+
 	/*
 	print_r($_GET);
 
