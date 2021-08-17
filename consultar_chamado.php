b8bb6048da62037349aed8ed25866bb8a91a1f65
unknown <vitomartinsalmeida@gmail.com>
2021-08-17 17:11:25 -0300

    Requisição de login nas outras paginas serase vai

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -82,7 +82,12 @@
   //abrir sessão
   session_start();
 
-  echo $SESSION['autenticado'];
+  //Checar se a sessão foi autenticada ou tem o valor diferente de SIM, se não for o caso, abrir tela de erro
+  if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
+
+    header('Location: index.php?login=erro2');
+
+  }
 
 ?>
 
