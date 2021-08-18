2793dcd686479b198d90e78315114a014ea95c8a
vitor1532 <86530351+vitor1532@users.noreply.github.com>
2021-08-18 15:33:49 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -32,13 +32,32 @@
               Login
             </div>
             <div class="card-body">
-              <form>
+              <form action="http://localhost/app_help_desk/valida_login.php" method="post">
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+                  <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
+                <? if (isset($_GET['login']) && $_GET['login'] == 'erro') {
+                ?>
+
+                <div class="text-danger">
+                  Usuário ou senha Inválido(s)
+                </div>
+
+                <? } ?>
+
+                <? if (isset($_GET['login']) && $_GET['login'] == 'erro2') {
+                ?>
+
+                <div class="text-danger">
+                  Faça login antes de acessar as páginas protegidas.
+                </div>
+
+                <? } ?>
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
---
+++ valida_login.php
@@ -0,0 +1,56 @@
+<?php
+
+	//iniciar o recurso de sessão
+	session_start();
+	
+	//variavel que verifica se a autenticação foi realizada
+	$usuario_autenticado = false;
+
+	//usuarios do sistema
+	$usuarios_app = [
+		array('email' => 'adm@teste.com', 'senha' => '123456'),
+		array('email' => 'user@teste.com', 'senha' => 'abcd'),
+		array('email' => 'vitor@teste.com', 'senha' => '456789'),
+	];
+
+	/*
+	echo '<pre>';
+	print_r($usuarios_app);
+	echo '</pre>';
+
+	print_r($_GET);
+	echo '<br>';
+	echo $_GET['email'];
+	echo '<br>';
+	echo $_GET['senha'];
+	*/
+
+	//percorre os arrays de $usuarios_app como $user comparando com os dados inseridos
+	foreach($usuarios_app as $user) {
+
+		if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+
+			$usuario_autenticado = true;
+
+		}
+
+	}
+
+	if($usuario_autenticado) {
+		echo 'Usuário autenticado.';
+		//caso autenticado, criar um indice na variavel de sessão
+		$_SESSION['autenticado'] = 'SIM';
+	} else {
+		$_SESSION['autenticado'] = 'NAO';
+		//força o redirecionamento
+		header('Location: index.php?login=erro');
+	}
+	//mudança1
+	/*
+	print_r($_POST);
+	echo '<br>';
+	echo $_POST['email'];
+	echo '<br>';
+	echo $_POST['senha'];
+	*/
+?>
