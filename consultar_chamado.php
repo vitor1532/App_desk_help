f89d4454678054c85c7689d9b72b9f53e10d2563
vitor1532 <vitomartinsalmeida@gmail.com>
2021-08-20 18:00:50 -0300

    cria os cartões via codigos e verifica se o array criado tem todas as informações

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -64,13 +64,21 @@
 
               <? foreach($chamados as $chamado) { ?>
 
-                <?= $chamado.'<br>' ?>
+                <?php
+
+                  $chamado_dados = explode('#', $chamado); // explode as informações que são retornados em arrays 
+
+                  if(count($chamado_dados) < 3) {//verifica se os arrays contém todos os dados
+                    continue;
+                  }
+
+                 ?>
               
                 <div class="card mb-3 bg-light">
                   <div class="card-body">
-                    <h5 class="card-title">Título do chamado...</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                    <p class="card-text">Descrição do chamado...</p>
+                    <h5 class="card-title"><?= $chamado_dados[0]; ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[1]; ?></h6>
+                    <p class="card-text"><?= $chamado_dados[2]; ?></p>
 
                   </div>
                 </div>
